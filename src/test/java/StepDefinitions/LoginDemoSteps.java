0aa3dd10395d0843557753d53db4424f4727efb6
wantinghsieh <wantinghsieh@gmail.com>
2022-04-26 20:42:17 -0400

    more

--- src/test/java/StepDefinitions/LoginDemoSteps.java
+++ src/test/java/StepDefinitions/LoginDemoSteps.java
@@ -15,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class LoginDemoSteps {
 	WebDriver driver = null;
 	
-	@Given("browser is open")
+	@Given("^browser is open$")
 	public void browser_is_open() {
 		System.out.println("Inside step - browser is open");
 		WebDriverManager.chromedriver().setup();
@@ -25,25 +25,25 @@ public class LoginDemoSteps {
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 	}
 
-	@And("user is on login page")
+	@And("^user is on login page$")
 	public void user_is_on_login_page() {
 		driver.navigate().to("https://example.testproject.io/web/"); 
 	}
 
-	@When("user enters username and password")
-	public void user_enters_username_and_password() {
-		driver.findElement(By.id("name")).sendKeys("wthsieh");
-		driver.findElement(By.id("password")).sendKeys("12345");
+	@When("^user enters (.*) and (.*)$")
+	public void user_enters_username_and_password(String username, String password) {
+		driver.findElement(By.id("name")).sendKeys(username);
+		driver.findElement(By.id("password")).sendKeys(password);
 	    
 	}
 
-	@And("user clicks on login")
+	@And("^user clicks on login$")
 	public void user_clicks_on_login() {
 		driver.findElement(By.id("login")).click();
 	    
 	}
 
-	@Then("user is navigated to the home page")
+	@Then("^user is navigated to the home page$")
 	public void user_is_navigated_to_the_home_page() {
 		driver.findElement(By.id("logout")).isDisplayed();
 		driver.close();
--- src/test/resources/Features/LoginDemo.feature
+++ src/test/resources/Features/LoginDemo.feature
@@ -1,8 +1,13 @@
 Feature: Test login functionality
 
-  Scenario: Check login is successful with valid credential
+  Scenario Outline: Check login is successful with valid credential
     Given browser is open
     And user is on login page
-    When user enters username and password
+    When user enters <username> and <password>
     And user clicks on login
     Then user is navigated to the home page
+
+    Examples: 
+      | username | password |
+      | wthsieh  |    12345 |
+      | Eft  |    12345 |
