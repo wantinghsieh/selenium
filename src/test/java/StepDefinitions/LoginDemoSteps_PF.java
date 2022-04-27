b8c8e652f4b50c8b1e72b1ba2076fc1731315d75
wantinghsieh <wantinghsieh@gmail.com>
2022-04-27 17:33:09 -0400

    more

--- src/test/java/StepDefinitions/LoginDemoSteps_PF.java
+++ src/test/java/StepDefinitions/LoginDemoSteps_PF.java
@@ -11,15 +11,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import pages.LoginPage;
+import pagefactory.HomePage_PF;
+import pagefactory.LoginPage_PF;
 
-public class LoginDemoSteps_POM {
-	/*
+public class LoginDemoSteps_PF {
+	
 	WebDriver driver = null;
-	LoginPage login = null;
+	LoginPage_PF login;
+	HomePage_PF home;
 	
 	@Given("^browser is open$")
 	public void browser_is_open() {
+		System.out.println("=== I am inside LoginDemoSteps_PF ===");
 		System.out.println("Inside step - browser is open");
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
@@ -36,7 +39,8 @@ public class LoginDemoSteps_POM {
 	@When("^user enters (.*) and (.*)$")
 	public void user_enters_username_and_password(String username, String password) {
 		
-		login = new LoginPage(driver);
+		login = new LoginPage_PF(driver);
+		
 		login.enterUsername(username);
 		login.enterPassword(password);
 		//driver.findElement(By.id("name")).sendKeys(username);
@@ -46,18 +50,19 @@ public class LoginDemoSteps_POM {
 
 	@And("^user clicks on login$")
 	public void user_clicks_on_login() {
-		login.clickLogin();
+		login.clickOnLogin();
 		//driver.findElement(By.id("login")).click();
 	    
 	}
 
 	@Then("^user is navigated to the home page$")
 	public void user_is_navigated_to_the_home_page() {
-		login.checkLogoutIsDisplayed();
+		home = new HomePage_PF(driver);
+		home.checkLogoutIsDisplayed();
 		//driver.findElement(By.id("logout")).isDisplayed();
 		driver.close();
 		driver.quit();
 	}
 
-*/
+
 }
---
+++ src/test/java/pagefactory/LoginPage_PF.java
@@ -0,0 +1,37 @@
+package pagefactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage_PF {
+	
+	@FindBy(id="name")
+	WebElement txt_username;
+	
+	@FindBy(id="password")
+	WebElement txt_password;
+	
+	@FindBy(id="login")
+	WebElement btn_login;
+	
+	WebDriver driver = null;
+	
+	public LoginPage_PF(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		
+	}
+	public void enterUsername(String username) {
+		txt_username.sendKeys(username);
+	}
+	
+	public void enterPassword(String password) {
+		txt_password.sendKeys(password);
+	}
+
+	public void clickOnLogin() {
+		btn_login.click();
+	}
+}
